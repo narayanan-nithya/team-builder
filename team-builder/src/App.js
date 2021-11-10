faa30e2802fd37768357c67bf6daca74c1a861cf
Nithya Narayanan <n.nithya@pm.me>
2021-11-09 16:46:39 -0800

    Added team form.

---
+++ team-builder/src/App.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './components/TeamForm.js'
+
+function App() {
+  // create State variables.
+  const []
+  //create and update forms. 
+
+  return (
+    <div className="App">
+      <h1> Team Builder App!</h1>
+    </div>
+  );
+}
+
+export default App;
